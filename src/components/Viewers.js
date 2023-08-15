8240d194e6adc10ea13809072f42729f23932b03
Lexi <106445147+restrepo-64@users.noreply.github.com>
2023-08-15 10:25:57 -0500

    added browser compatibility on home and viewers content with categories

--- src/components/Viewers.js
+++ src/components/Viewers.js
@@ -5,31 +5,31 @@ const Viewers = (props) => {
         <Container>
             <Wrap>
                 <img src='/images/viewers-disney.png' alt='' />
-                <video autoPlay loop playsInline>
+                <video autoPlay muted loop playsInline>
                     <source src='/videos/1564674844-disney.mp4' type='video/mp4' />
                 </video>
             </Wrap>
             <Wrap>
                 <img src='/images/viewers-marvel.png' alt='' />
-                <video autoPlay loop playsInline>
+                <video autoPlay muted loop playsInline>
                     <source src='/videos/1564676115-marvel.mp4' type='video/mp4' />
                 </video>
             </Wrap>
             <Wrap>
                 <img src='/images/viewers-national.png' alt='' />
-                <video autoPlay loop playsInline>
+                <video autoPlay muted loop playsInline>
                     <source src='/videos/1564676296-national-geographic.mp4' type='video/mp4' />
                 </video>
             </Wrap>
             <Wrap>
                 <img src='/images/viewers-pixar.png' alt='' />
-                <video autoPlay loop playsInline>
+                <video autoPlay muted loop playsInline>
                     <source src='/videos/1564676714-pixar.mp4' type='video/mp4' />
                 </video>
             </Wrap>
             <Wrap>
                 <img src='/images/viewers-starwars.png' alt='' />
-                <video autoPlay loop playsInline>
+                <video autoPlay muted loop playsInline>
                     <source src='/videos/1608229455-star-wars.mp4' type='video/mp4' />
                 </video>
             </Wrap>
